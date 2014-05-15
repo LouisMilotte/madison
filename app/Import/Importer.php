38912679116373e85a1aa080c85d3f3e724bbd61
Chris Birk <cmbirk@gmail.com>
2014-05-15 15:42:24 -0400

    tentatively finish document save to database in doc:import command

--- app/Import/Importer.php
+++ app/Import/Importer.php
@@ -40,6 +40,8 @@ class Importer
     }
 
     public function importFile($file){
+        $returnMessage = array();
+
         if(!file_exists($file)){
             throw new FileNotFoundException($file);
         }
@@ -48,14 +50,38 @@ class Importer
         $this->converter->setXML($xml);
 
         $body = $this->converter->getBody();
-        // $title = $this->converter->getTitle();
-        // $slug = $this->converter->createSlug($title);
-        // $sponsor = $this->converter->getSponsor();
-        // $status = $this->converter->getStatus();
-        // $committee = $this->converter->getCommittee();
-        
+        $title = $this->converter->getTitle();
+        $slug = $this->converter->createSlug($title);
+        $sponsor = $this->converter->getSponsor();
+        $status = $this->converter->getStatus();
+        $committee = $this->converter->getCommittee();
 
-        echo $body;
+        //Create Doc
+        try{
+            $doc = Doc::where('slug', $slug)->first();
+
+            //If this document already exists
+            if(isset($doc)){
+               $returnMessage['status'] = 'skipped';
+               $returnMessage['message'] = 'Document with slug ' . $slug . ' already exists.';
+               $returnMessage['id'] = $doc->id;
+            }else{
+                $doc_id = $this->saveNewDoc($title, $slug, $body);
+                $this->saveSponsor($sponsor, $doc_id);
+                $this->saveStatus($status, $doc_id);
+                $this->saveCommittee($committee, $doc_id);
+
+                $returnMessage['status'] = 'success';
+                $returnMessage['message'] = 'Document with slug ' . $slug . ' saved successfully';
+                $returnMessage['id'] = $doc_id;
+            }
+        }catch(Exception $e){
+            $returnMessage['status'] = 'error';
+            $returnMessage['message'] = $e->getMessage();
+            $returnMessage['id'] = null;
+        }
+        
+        return $returnMessage;
     }
 
     public function readContentsAsXML($filename){
@@ -81,5 +107,59 @@ class Importer
         $markdown = $this->converter->getBody();
 
         return $markdown;
+    }
+
+    protected function saveDoc($title, $slug, $body){
+        $doc = new Doc();
+        $doc->title = $title;
+        $doc->slug = $slug;
+        $doc->save();
+
+        $starter = new DocContent();
+        $starter->doc_id = $doc->id;
+        $starter->content = $body;
+        $starter->save();
+
+        $doc->init_section = $starter->id;
+        $doc->save();
+
+        return $doc->id;
+    }
+
+    protected function saveSponsor($sponsor, $doc_id){
+        //Save Doc Sponsor as DocMeta
+            if(isset($sponsor)){
+                $sponsorObject = new DocMeta();
+                $sponsorObject->doc_id = $doc_id;
+                $sponsorObject->meta_key = 'imported_sponsor';
+                $sponsorObject->meta_value = $sponsor;
+                return $sponsorObject->save();
+            }
+    }
+
+    protected function saveStatus($status, $doc_id){
+        //Save Doc Status
+        if(isset($status)){
+            $statusObject = Status::where('label', $status)->first();
+
+            if(!isset($statusObject)){
+                $statusObject = new Status();
+                $statusObject->label = $status;
+            }
+
+            $statusObject->save();
+            return $doc->statuses()->sync(array($statusObject->id));
+        }
+    }
+
+    protected function saveCommittee($committee, $doc_id){
+        //Save Doc Committee
+        if(isset($committee)){
+            $committeeObject = new DocMeta();
+            $committeeObject->doc_id = $doc->id;
+            $committeeObject->meta_key = 'imported_committee';
+            $committeeObject->meta_value = $committee;
+            return $committeeObject->save();
+        }
     }
 }
