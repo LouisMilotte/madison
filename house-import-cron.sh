b9d7b91a486695f6d78db204e66baf53c437f82a
Chris Birk <cmbirk@gmail.com>
2014-05-22 12:43:19 -0400

    fix relative links

--- house-import-cron.sh
+++ house-import-cron.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-php=`which php`
+PHP=`which php`
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-$php ./artisan db:backup
-$php ./artisan doc:download http://www.gpo.gov/fdsys/bulkdata/BILLS/113/2/hr/BILLS-113-2-hr.zip --bulk
-$php ./artisan doc:import app/storage/docs/xml/ --all
+$PHP $DIR/artisan db:backup
+$PHP $DIR/artisan doc:download http://www.gpo.gov/fdsys/bulkdata/BILLS/113/2/hr/BILLS-113-2-hr.zip --bulk
+$PHP $DIR/artisan doc:import $DIR/app/storage/docs/xml/ --all
