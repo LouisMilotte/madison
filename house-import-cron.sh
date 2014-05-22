df5dc4ca87f31df06c4d8b942b6131b8f185b17d
Chris Birk <cmbirk@gmail.com>
2014-05-22 01:12:01 -0400

    check php location for script

--- house-import-cron.sh
+++ house-import-cron.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-php artisan db:backup
-php artisan doc:download http://www.gpo.gov/fdsys/bulkdata/BILLS/113/2/hr/BILLS-113-2-hr.zip --bulk
-php artisan doc:import app/storage/docs/xml/ --all
+php=`which php`
+
+$php artisan db:backup
+$php artisan doc:download http://www.gpo.gov/fdsys/bulkdata/BILLS/113/2/hr/BILLS-113-2-hr.zip --bulk
+$php artisan doc:import app/storage/docs/xml/ --all
