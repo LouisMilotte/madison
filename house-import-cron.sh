d333d5c11ca81e9ca6ca5fb36a1fe1ea1a542a24
Chris Birk <cmbirk@gmail.com>
2014-05-22 01:13:38 -0400

    fix artisan location

--- house-import-cron.sh
+++ house-import-cron.sh
@@ -2,6 +2,6 @@
 
 php=`which php`
 
-$php artisan db:backup
-$php artisan doc:download http://www.gpo.gov/fdsys/bulkdata/BILLS/113/2/hr/BILLS-113-2-hr.zip --bulk
-$php artisan doc:import app/storage/docs/xml/ --all
+$php ./artisan db:backup
+$php ./artisan doc:download http://www.gpo.gov/fdsys/bulkdata/BILLS/113/2/hr/BILLS-113-2-hr.zip --bulk
+$php ./artisan doc:import app/storage/docs/xml/ --all
